6a678861f9001c6230db8c84c8b91cb62aea730c
Mr. Remark <itsmine096@gmail.com>
2021-06-08 16:31:00 +0900

    프로젝트(iOS, SpringBoot) 생성, .gitignore추가

---
+++ MashUp_SpringBoot/src/main/java/ac/kr/jejunu/mashup/MashUpApplication.java
@@ -0,0 +1,13 @@
+package ac.kr.jejunu.mashup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MashUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MashUpApplication.class, args);
+    }
+
+}
---
+++ MashUp_iOS/MashUp/MashUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MashUp
+//
+//  Created by Hyun on 2021/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
